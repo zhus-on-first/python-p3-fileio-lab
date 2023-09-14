55b726000fd7c05f1d398867a25491adb3cec576
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-14 10:38:38 -0700

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
-
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as log_file:
+        log_file.write(file_content)
+   
 def append_file(file_name, append_content):
-    pass
+     with open(f"{file_name}.txt", mode="a", encoding="utf-8") as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="utf-8") as log_file:
+        return log_file.read()
